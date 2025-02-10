9554d30a93d1518bc7d2360c6cafa7ac818af18c
HrunytskyiDS <dmytro.hrunytskyi@gmail.com>
2025-02-10 20:54:24 +0200

    feat(auth): add register and login
    - Added realization with JWT token

---
+++ src/auth/auth.constants.ts
@@ -0,0 +1,3 @@
+export const ALREADY_REGISTERED_ERROR = 'Такий користувач вже зареєстрований';
+export const USER_NOT_FOUND_ERROR = 'Користувача не знайдено';
+export const WRONG_PASSWORD_ERROR = 'Неправильний пароль';
